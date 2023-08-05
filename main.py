653cdb800008ae09facd02eb59b21f01c52bf956
Vagus-Truman <136032433+Vagus-Truman@users.noreply.github.com>
2023-08-05 17:02:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,3 @@ if __name__ == "__main__":
 # use this in terminal to get the webspace open
 # pip install llama-cpp-python[server]
 # $env:MODEL = "[Insert Model Path]"
-
-# $env:MODEL = "C:\Users\Kenne\Desktop\Ginga_AI\.bin files\llama-2-13b-chat.ggmlv3.q4_K_M.bin"
-# python -m llama_cpp.server
